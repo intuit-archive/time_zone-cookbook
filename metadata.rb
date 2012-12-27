07e908ccd3246b6ea90ca2cbb86085d9724507bc
Tommy Bishop <bishop.thomas@gmail.com>
2012-12-27 13:58:21 -0800

    tweak license and description in metadata

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
 name             'time_zone'
 maintainer       'Thomas Bishop'
 maintainer_email 'thomas_bishop@intuit.com'
-license          'All rights reserved'
-description      'Installs/Configures time_zone'
+license          'Apache 2.0'
+description      'Configures time_zone'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 recipe 'time_zone', 'Configures the OS time zone'
